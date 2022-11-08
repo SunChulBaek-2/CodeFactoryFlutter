1dc4c0b4117fa668dd10593db73e1ee5969056eb
x1210x <x1210x@gmail.com>
2022-11-08 13:36:13 +0900

    AppBar와 TabBar 만들기

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,14 +3,43 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
-  const DefaultLayout({this.backgroundColor, required this.child, super.key});
+  const DefaultLayout({
+    this.backgroundColor,
+    required this.child,
+    this.title,
+    this.bottomNavigationBar,
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if  (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500
+          )
+        )
+      );
+    }
   }
 }
