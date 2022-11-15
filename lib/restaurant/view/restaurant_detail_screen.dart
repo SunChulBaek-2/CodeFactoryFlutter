a93193081940a8d34e97ead3a6565e5d607b6e92
x1210x <x1210x@gmail.com>
2022-11-15 16:39:37 +0900

    SliverList 구현하기

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -13,15 +13,45 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard.fromModel(model: item, isDetail: true, detail: '맛있는 덖복이'),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: ProductCard()
-          )
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts()
         ],
       )
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+        child : RestaurantCard.fromModel(model: item, isDetail: true, detail: '맛있는 덖복이')
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverToBoxAdapter(
+        child: Text('메뉴', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18))
+      )
+    );
+  }
+
+  renderProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: ProductCard()
+            );
+          },
+          childCount: 10
+        )
+    )
     );
   }
 }
