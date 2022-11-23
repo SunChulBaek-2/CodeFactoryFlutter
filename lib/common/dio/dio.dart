fb652269415294c9e0532c67484dc248f210d8b1
x1210x <x1210x@gmail.com>
2022-11-23 10:02:57 +0900

    Dio onError Interceptor 작업하기

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -23,4 +23,38 @@ class CustomInterceptor extends Interceptor {
   // 2) 응답을 받을 때
 
   // 3) 에러가 났을 때
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    // 401 에러 -> 토큰 재발급 -> 재요청
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    if (refreshToken == null) {
+      return handler.reject(err);
+    }
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+    if (isStatus401 && !isPathRefresh) {
+      try {
+        final dio = Dio();
+        final response = await dio.post(
+            'http://$ip/auth/token',
+            options: Options(
+                headers: {
+                  'authrization': 'Bearer $refreshToken',
+                }
+            )
+        );
+        final accessToken = response.data['accessToken'];
+        final options = err.requestOptions;
+        options.headers.addAll({
+          'authrization': 'Bearer $accessToken',
+        });
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+        final response2 = await dio.fetch(options);
+        return handler.resolve(response2);
+      } on DioError catch (e) {
+        return handler.reject(err);
+      }
+    }
+    return handler.reject(err);
+  }
 }
