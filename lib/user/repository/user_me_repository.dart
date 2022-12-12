cb8eff4c3935a360f779b3dabdc448cefa3f6a64
x1210x <x1210x@gmail.com>
2022-12-12 10:10:39 +0900

    UserMeRepository 생성하기

---
+++ lib/user/repository/user_me_repository.dart
@@ -0,0 +1,26 @@
+import 'package:codefactory_flutter/common/const/data.dart';
+import 'package:codefactory_flutter/common/dio/dio.dart';
+import 'package:codefactory_flutter/user/model/user_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/http.dart';
+
+part 'user_me_repository.g.dart';
+
+final userMeRepositoryProvider = Provider<UserMeRepository>(
+    (ref) {
+      final dio = ref.watch(dioProvider);
+      return UserMeRepository(dio, baseUrl: 'http://$ip/user/me');
+    }
+);
+
+@RestApi()
+abstract class UserMeRepository {
+  factory UserMeRepository(Dio dio, {String baseUrl}) = _UserMeRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken:':'true',
+  })
+  Future<UserModel> getMe();
+}
